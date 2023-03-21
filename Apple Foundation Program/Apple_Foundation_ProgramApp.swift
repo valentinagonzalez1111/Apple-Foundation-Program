8e0c152ba1f46ba06bc45bddd562d5b441c7beeb
Valentina <balendi.art@gmail.com>
2023-03-21 10:04:15 +0100

    Initial Commit

---
+++ Apple Foundation Program/Apple_Foundation_ProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Foundation_ProgramApp.swift
+//  Apple Foundation Program
+//
+//  Created by Apprenant 18 on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Foundation_ProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
